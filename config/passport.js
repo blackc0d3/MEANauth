60416cd3e29066615c056db2c3650abdc9139363
Jorge Avila <javila83@gmail.com>
2017-08-21 21:20:56 +0200

    Backend auth completed

---
+++ config/passport.js
@@ -0,0 +1,23 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+const config = require('../config/database');
+
+module.exports = function(passport){
+  let opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+  opts.secretOrKey = config.secret;
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log(jwt_payload);
+      User.findOne({id: jwt_payload.id}, (err, user) => {
+          if (err) {
+              return done(err, false);
+          }
+          if (user) {
+              done(null, user);
+          } else {
+              done(null, false);
+          }
+      });
+  }));
+};
