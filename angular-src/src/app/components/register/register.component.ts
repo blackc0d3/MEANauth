c8e3cfa778a2857ce44b013903fecdd9743f13d2
Jorge Avila <javila83@gmail.com>
2017-08-22 02:46:03 +0200

    Flash messages and registration/API working

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -13,7 +17,12 @@ export class RegisterComponent implements OnInit {
   password: String;
 
 
-  constructor(private validateService: ValidateService) { }
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -27,14 +36,25 @@ export class RegisterComponent implements OnInit {
     }
     // Required Fields
     if(!this.validateService.validateRegister(user)){
-      console.log('Please fill in all fields');
+      this.flashMessage.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000})
       return false;
     }
     // Validate Email
     if(!this.validateService.validateEmail(user.email)){
-      console.log('Please use a valid email');
+      this.flashMessage.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000})
       return false;
     }
+
+    // Register user
+    this.authService.registerUser(user).subscribe(data => {
+      if(data.success){
+        this.flashMessage.show('You are now registered and can log in', {cssClass: 'alert-success', timeout: 3000})
+        this.router.navigate(['/login'])
+      } else {
+        this.flashMessage.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000})
+        this.router.navigate(['/register'])
+      }
+    })
   }
 
 }
