b9465a09e6e190a9cd91100c1dbc2e640cec530e
Jorge Avila <javila83@gmail.com>
2017-08-23 00:23:51 +0200

    linked db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database: 'mongodb://<dbuser>:<dbpassword>@ds023463.mlab.com:23463/heroku_bxc5pm6m', // Heroku Deployment
+  database: 'mongodb://javila83:Molotov1@ds155473.mlab.com:55473/meanauth', // Heroku Deployment
   // database: 'mongodb://localhost:27017/meanauth',
   secret: 'meanauthsecret'
 };
