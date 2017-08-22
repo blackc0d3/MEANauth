16b8da3241d1496f5293c63972c8add7cebae708
Jorge Avila <javila83@gmail.com>
2017-08-23 00:11:37 +0200

    deployment

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://<dbuser>:<dbpassword>@ds023463.mlab.com:23463/heroku_bxc5pm6m', // Heroku Deployment
+  // database: 'mongodb://localhost:27017/meanauth',
   secret: 'meanauthsecret'
 };
